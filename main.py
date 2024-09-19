8cd5933407ba55fb128370e3f4765502777bebe9
Gee2563 <george.farhatsmith@gmail.com>
2024-09-19 17:23:44 +0100

    Refactor date calculation in main.py for fetching historical market data

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from utils.data_utils import fetch_historical_market_data
 
 def main():
     print("Starting the training process...")
-    end_date = (datetime.datetime.now()- datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    start = datetime.datetime.now() - datetime.timedelta(days=2)
+    end_date = start.strftime('%Y-%m-%d')
     #end date = five years ago today
-    start_date = (end_date - datetime.timedelta(days=5*365)).strftime('%Y-%m-%d')
+    start_date = (start - datetime.timedelta(days=5*365)).strftime('%Y-%m-%d')
     print(f"Fetching data from {start_date} to {end_date}")
     data = fetch_historical_market_data('binance', 'BTC/USDT', timeframe='1m', start_date=start_date, end_date=end_date)
     train_model(data)
